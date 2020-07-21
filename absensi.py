968a76a4c954f12a690165f18d789ee71a86f60e
regnents <rafaelseanputra@gmail.com>
2020-07-21 12:27:37 +0700

    create function sheet_editor

--- absensi.py
+++ absensi.py
@@ -5,7 +5,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 
-
+# Preparing necessary variable
+daftar_hasil =[]
+checker = []
 
 def second_counter(waktu):
     h, m, s = waktu.split(":")
@@ -13,7 +15,62 @@ def second_counter(waktu):
     return detik
 
 def sheet_editor():
-    pass
+    global daftar_hasil
+    global checker
+    scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+
+    file_name = "client_key.json"
+    creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+    client = gspread.authorize(creds)
+
+    sheet_presensi = client.open("Presensi Peserta SPARTA 2019 v2")
+    chosen_work = sheet_presensi.worksheet("Presensi")
+
+    # Finding the column that need to be edited
+    temp_cell_number = chosen_work.find(day)
+    col_number = temp_cell_number.col
+    temp_row_number = temp_cell_number.row + 1
+    cell_data = chosen_work.cell(temp_row_number,col_number).value
+    while cell_data != sesi:
+        col_number += 1
+        cell_data = chosen_work.cell(temp_row_number,col_number).value
+
+    # Finding the row and edit the cell in the exact row and col
+    start_time = time.time()
+    for i in range (len(daftar_hasil)):
+        if (checker[i] != 1):
+            pass
+        else:
+            temp_nim = re.search("16519\d\d\d",daftar_hasil[i]).group(0)
+            nim = temp_nim[-3:]
+            kel = re.search("\d\d ",daftar_hasil[i]).group(0)
+            kel = re.sub(" ","",kel)
+            nim_cell = chosen_work.findall(nim)
+            while (len(nim_cell) > 1):
+                j = 0
+                while j < len(nim_cell):
+                    if (nim_cell[j].col != 1):
+                        nim_cell.remove(nim_cell[j])
+                    else:
+                        j += 1
+            if (len(nim_cell) == 0):
+                print(nim + " gak ada di sheet presensi")
+                checker[i] = -1
+            else:
+                row_number = nim_cell[0].row
+                kel_cell_value =  chosen_work.cell(row_number,3).value
+                if (int(kel_cell_value) != int(kel)):
+                    print(nim + " ada tapi kelompoknya salah")
+                    checker[i] = -1
+                else:
+                    chosen_work.update_cell(row_number,col_number,"1")
+        if (i % 10 == 0):
+            time.sleep(10 - ((time.time() - start_time) % 10))
+            start_time = time.time()
+
 
 
 
@@ -22,7 +79,6 @@ def sheet_editor():
 file_chat = input("Masukkan nama file chat ZOOM (Pastikan file berada di folder file-chat dan jangan lupa extensi .txt nya): ")
 file_chat = "file-chat/" + file_chat
 daftar_teks = open(file_chat,"r",encoding='utf-8')
-daftar_hasil =[]
 
 # Getting all the data (day, sesi, waktu absensi)
 day = "Day " + str(int(input("Day ke berapa? ")))
@@ -77,71 +133,26 @@ for i in range (len(daftar_hasil)):
     daftar_hasil[i] = re.sub("\n"," ",temp)
 
 # Check if chat is about presence or not
-checker = []
 for i in range (len(daftar_hasil)):
-    temp = re.findall("\d\d 16519\d\d\d",daftar_hasil[i])
-    if (len(temp) != 2):
+    temp_kel = re.findall(" \d\d ",daftar_hasil[i])
+    temp_nim = re.findall("16519\d\d\d",daftar_hasil[i])
+    if (len(temp_kel) != 2) and (len(temp_nim) != 2):
         checker.append(0)
-    elif (temp[0] != temp[1]):
+    elif (temp_kel[0] != temp_kel[1]) and (temp_nim[0] != temp_nim[1]):
         checker.append(0)
-        print("Presensi dan username berbeda untuk username" + temp[0].group(0))
+        print("Presensi dan username berbeda untuk username " + temp_kel[0] + " " + temp_nim[0])
     else:
         checker.append(1)
 
-# Open and enter the spreadsheet
-scope = [
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.file'
-]
-
-file_name = "client_key.json"
-creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
-client = gspread.authorize(creds)
-
-sheet_presensi = client.open("Presensi Peserta SPARTA 2019 v2")
-chosen_work = sheet_presensi.worksheet("Presensi")
-
-# Finding the column that need to be edited
-temp_cell_number = chosen_work.find(day)
-col_number = temp_cell_number.col
-temp_row_number = temp_cell_number.row + 1
-cell_data = chosen_work.cell(temp_row_number,col_number).value
-while cell_data != sesi:
-    col_number += 1
-    cell_data = chosen_work.cell(temp_row_number,col_number).value
-
-# Finding the row and edit the cell in the exact row and col
-start_time = time.time()
-for i in range (len(daftar_hasil)):
-    if (checker[i] != 1):
-        pass
-    else:
-        temp_nim = re.search("16519\d\d\d",daftar_hasil[i]).group(0)
-        nim = temp_nim[-3:]
-        kel = re.search("\d\d ",daftar_hasil[i]).group(0)
-        kel = re.sub(" ","",kel)
-        nim_cell = chosen_work.findall(nim)
-        while (len(nim_cell) > 1):
-            j = 0
-            while j < len(nim_cell):
-                if (nim_cell[j].col != 1):
-                    nim_cell.remove(nim_cell[j])
-                else:
-                    j += 1
-        if (len(nim_cell) == 0):
-            print(nim + " gak ada di sheet presensi")
-            checker[i] = -1
-        else:
-            row_number = nim_cell[0].row
-            kel_cell_value =  chosen_work.cell(row_number,3).value
-            if (int(kel_cell_value) != int(kel)):
-                print(nim + " ada tapi kelompoknya salah")
-                checker[i] = -1
-            else:
-                chosen_work.update_cell(row_number,col_number,"1")
-    if (i % 10 == 0):
-        time.sleep(10 - ((time.time() - start_time) % 10))
-        start_time = time.time()
+# Open and edit the spreadsheet
+done = 0
+while not done:
+    try:
+        sheet_editor()
+        done = 1
+    except Exception as err:
+        print(format(err))
+        time.sleep(5)
         
 # Writing the result in a txt file
 nama_file = "hasil-absensi/absensi " + day + " " + sesi +".txt"
